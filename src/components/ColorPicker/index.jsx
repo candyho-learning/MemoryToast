c32e03fa3ed5d1fe8037f4c3ab78860eed44ac62
yili <hellomickey004@gmail.com>
2024-03-30 16:04:37 +0800

    add color chip, hex color

--- src/components/ColorPicker/index.jsx
+++ src/components/ColorPicker/index.jsx
@@ -1,9 +1,41 @@
 import React, { useState, Fragment, useEffect, useRef } from 'react';
 import Wheel from '@uiw/react-color-wheel';
 import { hsvaToHex } from '@uiw/color-convert';
+import styled from 'styled-components';
+const ColorPickerWrapper = styled.div`
+`;
 
+const PanetoneWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 150px;
+`;
+const ColorDiv = styled.div`
+  width: 100%;
+  background-color: #fff;
+  height: 140px;
+`;
+const WhiteDiv = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: #fff;
+  height: 90px;
+  padding: 8px;
+  p:nth-of-type(1) {
+    font-weight: bold;
+    font-size: 20px;
+  }
+  p {
+    margin: 0 !important;
+    color: black;
+    font-size: 14px;
+  }
+`;
 function ColorPicker({ setColorName, colorName, setColorCode }) {
   const [hsva, setHsva] = useState({ h: 214, s: 43, v: 90, a: 1 });
+  const [hex, setHex] = useState('');
   // const [colorName, setColorName] = useState("");
   const isLoading = useRef(false);
   const firstLoading = useRef(true);
@@ -12,7 +44,6 @@ function ColorPicker({ setColorName, colorName, setColorCode }) {
     if (isLoading.current) return;
 
     timerId = setTimeout(async () => {
-      
       if (firstLoading.current) {
         firstLoading.current = false;
         return;
@@ -21,9 +52,9 @@ function ColorPicker({ setColorName, colorName, setColorCode }) {
       const colorData = await getColorDataFromHSV();
       isLoading.current = false;
       const colorName = colorData.name.value; //color name
-      const colorHEX = colorData.hex.value; //rgb color code
-      console.log(colorName, colorHEX);
+      const colorHEX = colorData.hex.value; //hex color code
       setColorCode(colorHEX);
+      setHex(colorHEX);
       setColorName(colorName);
     }, 50);
 
@@ -53,22 +84,24 @@ function ColorPicker({ setColorName, colorName, setColorCode }) {
   }
 
   return (
-    <Fragment>
+    <ColorPickerWrapper>
       <Wheel
         color={hsva}
         onChange={(color) => setHsva({ ...hsva, ...color.hsva })}
       />
-      <div
-        style={{
-          width: '100%',
-          height: 34,
-          marginTop: 20,
-          background: hsvaToHex(hsva),
-        }}
-      >
-        <p>{colorName}</p>
-      </div>
-    </Fragment>
+      <PanetoneWrapper>
+        <ColorDiv
+          style={{
+            background: hsvaToHex(hsva),
+          }}
+        ></ColorDiv>
+        <WhiteDiv>
+          <p>Lucky Color</p>
+          <p>{hex}</p>
+          <p>{colorName}</p>
+        </WhiteDiv>
+      </PanetoneWrapper>
+    </ColorPickerWrapper>
   );
 }
 
