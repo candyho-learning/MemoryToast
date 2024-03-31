fd30ad5b063164e76ecc08430e981268b4a44788
yili <hellomickey004@gmail.com>
2024-03-31 15:58:42 +0800

    init

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -6,56 +6,91 @@ export const AuthContext = createContext({
   isLogin: false,
   user: {},
   loading: false,
-  jwtToken: "",
+  accessToken: "",
   login: () => {},
   logout: () => {},
 });
 
 export const AuthContextProvider = ({ children }) => {
   const [isLogin, setIsLogin] = useState(false);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(() => {
+    return JSON.parse(localStorage.getItem("userProfile")) || undefined;
+  });
   const [loading, setLoading] = useState(true);
-  const [jwtToken, setJwtToken] = useState();
+  const [accessToken, setAccessToken] = useState(() => {
+    return JSON.parse(localStorage.getItem("accessToken")) || undefined;
+  });
 
-  const handleLoginResponse = useCallback(async (response) => {
-    const accessToken = response.authResponse.accessToken;
-    const { data } = await api.signin({
-      provider: "facebook",
-      access_token: accessToken,
-    });
-    const { access_token: tokenFromServer, user: userData } = data;
-    setUser(userData);
-    setJwtToken(tokenFromServer);
-    window.localStorage.setItem("jwtToken", tokenFromServer);
-    setIsLogin(true);
-    return tokenFromServer;
-  }, []);
+  // const handleLoginResponse = useCallback(async (response) => {
+  //   const accessToken = response.authResponse.accessToken;
+  //   const { data } = await api.signin({
+  //     provider: "facebook",
+  //     access_token: accessToken,
+  //   });
+  //   const { access_token: tokenFromServer, user: userData } = data;
+  //   setUser(userData);
+  //   setAccessToken(tokenFromServer);
+  //   window.localStorage.setItem("accessToken", tokenFromServer);
+  //   setIsLogin(true);
+  //   return tokenFromServer;
+  // }, []);
 
   useEffect(() => {
+    console.log("validating token...");
+    console.log(accessToken);
+    if (!accessToken) {
+      console.log("no token, aborting");
+      setLoading(false);
+      return;
+    }
     const checkAuthStatus = async () => {
-      await fb.init();
-      const response = await fb.getLoginStatus();
-      if (response.status === "connected") {
-        handleLoginResponse(response);
+      const response = await api.getProfile(accessToken);
+      const data = response?.data ?? null;
+
+      console.log(data);
+      if (data) {
+        setIsLogin(true);
         setLoading(false);
+        setUser(data);
       } else {
-        window.localStorage.removeItem("jwtToken");
+        console.log("token invalid");
+        window.localStorage.removeItem("accessToken");
+        window.localStorage.removeItem("userProfile");
         setLoading(false);
       }
     };
     checkAuthStatus();
   }, []);
 
-  const login = async () => {
+  // const login = async () => {
+  //   setLoading(true);
+  //   // TODO: Send data to Login API
+  //   const response = await fb.login();
+  //   if (response.status === "connected") {
+  //     const tokenFromServer = handleLoginResponse(response);
+  //     setLoading(false);
+  //     return tokenFromServer;
+  //   } else {
+  //     window.localStorage.removeItem("accessToken");
+  //     setLoading(false);
+  //     return null;
+  //   }
+  // };
+
+  const login = async (loginData) => {
     setLoading(true);
     // TODO: Send data to Login API
-    const response = await fb.login();
-    if (response.status === "connected") {
-      const tokenFromServer = handleLoginResponse(response);
+    const { data } = await api.signin(loginData);
+    if (data) {
       setLoading(false);
-      return tokenFromServer;
+      setIsLogin(true);
+      localStorage.setItem("accessToken", JSON.stringify(data.access_token));
+      localStorage.setItem("userProfile", JSON.stringify(data.user));
+      return data;
     } else {
-      window.localStorage.removeItem("jwtToken");
+      console.log("log in failed");
+      window.localStorage.removeItem("accessToken");
+      window.localStorage.removeItem("userProfile");
       setLoading(false);
       return null;
     }
@@ -63,11 +98,10 @@ export const AuthContextProvider = ({ children }) => {
 
   const logout = async () => {
     setLoading(true);
-    await fb.logout();
     setIsLogin(false);
     setUser({});
-    setJwtToken();
-    window.localStorage.removeItem("jwtToken");
+    setAccessToken();
+    window.localStorage.removeItem("accessToken");
     setLoading(false);
   };
 
@@ -77,7 +111,7 @@ export const AuthContextProvider = ({ children }) => {
         isLogin,
         user,
         loading,
-        jwtToken,
+        accessToken,
         login,
         logout,
       }}
